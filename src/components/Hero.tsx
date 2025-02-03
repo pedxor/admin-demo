65154117bbe7c84e525a7b0b1be1aa816bf273c7
Pedro Blasco <pedxor@gmail.com>
2025-02-03 12:02:18 +0100

    Ajustes Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,22 +4,26 @@ import React from "react";
 
 const Hero: React.FC = () => {
   return (
-    <figure className="relative flex justify-center h-full flex-col overflow-hidden rounded-lg bg-white p-7">
+    <figure className="relative flex flex-col justify-center overflow-hidden rounded-lg bg-white p-7">
       <h1 className="mb-5">Welcome back, Angelina</h1>
       <p className="mb-4 font-medium">
-        You've completed <span className="font-bold text-primary">60%</span> of
+        You've completed <span className="text-primary font-bold">60%</span> of
         your tasks for today. <br />
         Your progress is{" "}
-        <span className="font-bold text-primary">very good!</span>
+        <span className="text-primary font-bold">very good!</span>
       </p>
       <div className="mb-5">
-        <button className="flex items-center justify-between gap-3 rounded-full bg-primary px-6 py-2 font-medium text-white">
+        <button className="bg-primary flex items-center justify-between gap-3 rounded-full px-6 py-2 font-medium text-white">
           <LuSquareCheck />
           Start completing today's tasks
         </button>
       </div>
       <div className="absolute top-0 right-0 h-full w-auto pt-5">
-        <img src={image} alt="Hero image" className="h-full w-full object-cover" />
+        <img
+          src={image}
+          alt="Hero image"
+          className="h-full w-full object-cover"
+        />
       </div>
     </figure>
   );
